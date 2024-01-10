70245d6e0f2c58a6c5551b35a94dc256c4b5b8ac
Danek5 <danek.trombik@gmail.com>
2024-01-10 14:20:59 +0100

    Přidejte soubory projektu.

---
+++ IMS.CoreBusiness/Inventory.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IMS.CoreBusiness
+{
+    public class Inventory
+    {
+        public int InventoryId { get; set; }
+        [Required]
+        public string? InventoryName { get; set; }
+        [Range(0, int.MaxValue, ErrorMessage = "Quantity must be grater or equal to {0}")]
+        public int Quantity { get; set; }
+        [Range(0, int.MaxValue, ErrorMessage = "Quantity must be grater or equal to {0}")]
+
+        public double Price { get; set; }
+    }
+}
---
+++ IMS.Plugins.EFCore/IMSContext.cs
@@ -0,0 +1,31 @@
+﻿using IMS.CoreBusiness;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Plugins.EFCore
+{
+    public class IMSContext : DbContext
+    {
+        public IMSContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Inventory> Inventories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Inventory>().HasData(
+            new Inventory { InventoryId = 1, InventoryName = "Electonics", Price = 22, Quantity = 100 },
+            new Inventory { InventoryId = 2, InventoryName = "Fruits", Price = 222, Quantity = 100 },
+            new Inventory { InventoryId = 3, InventoryName = "Meat", Price = 220, Quantity = 100 },
+            new Inventory { InventoryId = 4, InventoryName = "Body", Price = 122, Quantity = 100 },
+            new Inventory { InventoryId = 5, InventoryName = "Engine", Price = 2, Quantity = 100 }
+            );
+        }
+    }
+}
